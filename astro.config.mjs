48c0a30c5257e8b7de93d413c91baf9748264774
John Dykes <98johndykes@gmail.com>
2025-07-25 00:04:43 -0400

    simplfiy tsparticles usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4444,
+    // remote cloudflare tunnel for testing on safari browser on MacOS
+    allowedHosts: ['housewives-developed-assurance-batman.trycloudflare.com']
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 });
